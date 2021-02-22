52cb0f4005d6d372f1a344b356529fb4bbe5318e
RGPRafael <40923283+RGPRafael@users.noreply.github.com>
2021-02-22 15:53:03 -0300

    Add files via upload

---
+++ MAC350/etapa3/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Usuario, Perfil,Servico,Exame,Pessoa
+
+admin.site.register(Usuario)
+admin.site.register(Perfil)
+admin.site.register(Servico)
+admin.site.register(Exame)
+admin.site.register(Pessoa)
---
+++ MAC350/etapa3/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Etapa3Config(AppConfig):
+    name = 'etapa3'
